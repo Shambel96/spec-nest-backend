f6fd15cd3050792821dbd79fa06df73cfdf85599
Shambel Dechu <shambeldechu@gmail.com>
2026-03-02 23:10:27 +0300

    feat: add authGuards and roles guard

--- nest-app/src/users/users.controller.ts
+++ nest-app/src/users/users.controller.ts
@@ -9,30 +9,34 @@ import {
   ParseIntPipe,
   HttpCode,
   HttpStatus,
+  UseGuards,
 } from '@nestjs/common';
 
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { AuthGuard } from '@nestjs/passport';
+import { RolesGuard } from '../auth/roles.guard';
 
 @Controller('users')
+@UseGuards(AuthGuard('jwt')) 
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
-  @HttpCode(HttpStatus.CREATED) 
+  @HttpCode(HttpStatus.CREATED)
   create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
   }
 
   @Get()
   findAll() {
-    return this.usersService.findAll(); 
+    return this.usersService.findAll();
   }
 
   @Get(':id')
   findOne(@Param('id', ParseIntPipe) id: number) {
-    return this.usersService.findOne(id); 
+    return this.usersService.findOne(id);
   }
 
   @Patch(':id')
@@ -43,7 +47,9 @@ export class UsersController {
     return this.usersService.update(id, updateUserDto);
   }
 
+  // Admin Only
   @Delete(':id')
+  @UseGuards(RolesGuard)
   @HttpCode(HttpStatus.NO_CONTENT)
   async remove(@Param('id', ParseIntPipe) id: number) {
     await this.usersService.remove(id);
