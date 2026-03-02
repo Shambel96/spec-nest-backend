c712a623ee523fd323c96877bd3491ebf2bf2d9b
Shambel Dechu <shambeldechu@gmail.com>
2026-03-02 23:08:11 +0300

    feat: add controller, module, and service auth with jwt and roles

---
+++ nest-app/src/auth/auth.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+import { JwtStrategy } from './jwt.strategy';
+import { JwtModule } from '@nestjs/jwt';
+import { PrismaService } from '../prisma/prisma.service';
+
+@Module({
+  imports: [
+    JwtModule.register({
+      secret: 'SECRET_KEY',
+      signOptions: { expiresIn: '1h' },
+    }),
+  ],
+  controllers: [AuthController],
+  providers: [AuthService, JwtStrategy, PrismaService],
+})
+export class AuthModule {}
